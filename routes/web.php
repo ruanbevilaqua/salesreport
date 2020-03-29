eb3b842330dff4c05d8f50f31cb059cac894f2f3
Ruan <ruan@wf5.com.br>
2020-03-29 16:42:38 -0300

    Rotas definidas para os controllers

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('client', 'ClientController');
+
+Route::resource('order', 'OrderController');
+
+Route::resource('payment', 'PaymentController');
+
+Route::resource('product', 'ProductController');
