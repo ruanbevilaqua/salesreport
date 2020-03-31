5104a28256f89a37af9e325b7d74a081aea1bb8c
Ruan <ruan@wf5.com.br>
2020-03-31 13:59:10 -0300

    Função para 'piscar' o menu para auxiliar o usuário a encontrar o menu ¯\_(ツ)_/¯

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -3,7 +3,7 @@
     <title>Relatório</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" id="navbar">
     <a class="navbar-brand" href="{{ url('/') }}">Salesreport</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
@@ -61,4 +61,6 @@
             document.body.innerHTML = originalPage;
         }
     </script>
+
+    @yield('js')
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,13 +6,15 @@
     <div class="row">
         <div class="col-md-4">
         <h2>Atenção</h2>
-        <p>O objetivo deste sistema é visualizar e exportar informações referentes a pedidos. Não telas para gerenciar todas as entidades existentes no sistema, contudo você pode utilizar qualquer cliente de API REST (Postman ou Imsomnia, por exemplo) e incluir novos dados.</p>
-        <p><a class="btn btn-secondary" href="#" role="button">Quero ver »</a></p>
+        <p>O objetivo deste sistema é visualizar e exportar informações referentes a pedidos.
+        Para uma melhor experiência, também foi implementado métodos e telas para a criação de usuários, clientes, produtos e pedidos 
+        Você pode acessar tudo utilizando o menu superior. Legal né?
+        </p>
+        <p><button class="btn btn-secondary" onclick="blink()">Aonde? </button></p>
         </div>
         <div class="col-md-4">
-        <h2>Head 2</h2>
-        <p> </p>
-        <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
+        <h2>Impressão de dados</h2>
+        <p>Você pode imprimir qualquer página acessada! É só utilizar o botão Imprimir que você encontra embaixo do conteúdo em todas as páginas</p>
         </div>
         <div class="col-md-4">
         <h2>Head 3</h2>
@@ -20,4 +22,22 @@
         <p><a class="btn btn-secondary" href="#" role="button">View details »</a></p>
         </div>
     </div>
+@endsection
+
+@section('js')
+<script>
+    function blink()
+    {
+        // original = document.getElementById('navbart').class
+        console.log('Blink!')
+        document.getElementById('navbar').classList.remove('bg-dark')
+        document.getElementById('navbar').classList.add('bg-light')
+        setTimeout(function(){ 
+            document.getElementById('navbar').classList.remove('bg-light')
+            document.getElementById('navbar').classList.add('bg-dark')    
+        }, 200);
+        
+        
+    }
+</script>
 @endsection
